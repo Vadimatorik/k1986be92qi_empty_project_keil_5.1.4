489659cd64b52c07b89ab922a7c318bc7c18dc73
Vadimatorik <Vadimatorikda@gmail.com>
2019-04-27 19:11:25 +0700

    Порт из облака.

---
+++ main.c
@@ -0,0 +1,14 @@
+#include "MDR32F9Qx_config.h"
+#include "MDR32Fx.h"
+#include "MDR32F9Qx_port.h"
+#include "MDR32F9Qx_rst_clk.h"
+#include "MDR32F9Qx_ssp.h"
+int main (void)
+{
+  while (1)
+  {
+		
+  }
+}
+
+
